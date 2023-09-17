7721f9bacc45d56381a5aa8d0520738ee3cab18e
Sawanjkotian <sawan9kotian@gmail.com>
2023-09-17 19:14:48 +0530

    app.js

--- app.js
+++ app.js
@@ -1,65 +1,40 @@
+
+const morgan = require('morgan');
 const express = require('express');
-const fs = require('fs');
+
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
 app.use(express.json());
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-
-app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status:"success",
-        results:tours.length,
-        data:{
-            tours
-        }
-    });
+app.use((req, res, next)=>{
+    console.log('Hello from the middleware');
+    next();
 });
 
-app.post('/api/v1/tours', (req, res) => {
-    //console.log(req.body);
-
-    const newId = tours[tours.length - 1].id +1;
-    const newTour = Object.assign({id: newId}, req.body);
-
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), err=>{
-        res.status(201).json({
-            status:'success',
-            data:{
-                tour: newTour
-            }
-        });
-    });
-});
-
-
-app.get('/api/v1/tours/:id', (req, res) => {
-        console.log(req.params);
-
-        const id = req.params.id * 1;
-
-        if(id > tours.length){
-            return res.status(404).json({
-                status:'fail',
-                message: 'Invalid Id'
-            });
-        }
-
-        const tour = tours.find(el=> el.id === id)
-
-        res.status(201).json({
-            status:'success',
-            data:{
-                tour
-            }
-        });
-    });
+app.use((req, res, next)=>{
+    req.requestTime = new Date().toISOString();
+    next();
+})
 
 
 
-const port = 3000;
 
-app.listen(port, ()=>{
-    console.log(`App running on port ${port}.....`);
-});
+// app.get('/api/v1/tours',getAllTours);
+// app.get('/api/v1/tours/:id',getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id',updateTour)
+
+// app.delete('/api/v1/tours/:id',deleteTour);
+
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+
+
+//Start server
+module.exports = app;
---
+++ routes/tourRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const tourController = require('./../controllers/tourController.js');
+const router = express.Router();
+
+
+router.param('id', tourController.CheckID);
+
+
+router
+    .route('/')
+    .get(tourController.getAllTours)
+    .post(tourController.CheckBody,tourController.createTour);
+
+router
+    .route('/:id')
+    .get(tourController.getTour)
+    .patch(tourController.updateTour)
+    .delete(tourController.deleteTour);
+
+    module.exports = router;
