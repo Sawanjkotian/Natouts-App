1383120d77f048a0cbbda3fb3bc428892a84c411
Sawanjkotian <sawan9kotian@gmail.com>
2023-10-01 16:24:15 +0530

    Aggregate Pipelines

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,6 +5,14 @@ const router = express.Router();
 
 // router.param('id', tourController.CheckID);
 
+router.route('/top-5-cheap')
+    .get(tourController.aliasTopTours, tourController.getAllTours)
+
+router.route('/tour-stats')
+    .get(tourController.getTourStats);
+
+router.route('/monthly-plan/:year')
+    .get(tourController.getMonthlyPlan);
 
 router
     .route('/')
@@ -12,7 +20,7 @@ router
     .post(tourController.createTour);
 
 router
-    .route('/:id')
+    .route('/:id')  
     .get(tourController.getTour)
     .patch(tourController.updateTour)
     .delete(tourController.deleteTour);
